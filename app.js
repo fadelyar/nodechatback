b9b507c4950164199510f12014c1daf4d9741474
Faisal <adelyarfaisal@gmail.com>
2021-05-19 14:56:56 +0430

    cors added

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const http = require('http')
 const app = express()
 const userRouter = require('./routes/user')
+const cors = require('cors')
+
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use('/user/',userRouter)
+app.use('/user/', userRouter)
 
 http.createServer(app).listen(2000, () => {
 	console.log('server is running on port 2000!')
