d1cfea0eda93dd3ee8c1c6c83c2cd6823f5dde81
Faisal <adelyarfaisal@gmail.com>
2021-05-26 21:19:14 +0430

    chat route added

--- routes/chat.js
+++ routes/chat.js
@@ -6,28 +6,30 @@ const prisma = new PrismaClient()
 
 router.get('/getlast30messages/:room', async (req,
 															res) => {
-		// const messages = await prisma.messages.findMany({
-		// 	take: 30,
-		// 	where: {
-		// 		group: {
-		// 			name: req.params.room
-		// 		}
-		// 	},
-		// 	select: {
-		// 		content: true,
-		// 		dateCreated: true,
-		// 		user: {
-		// 			select: {
-		// 				name: true
-		// 			}
-		// 		}
-		// 	},
-		// 	orderBy: {
-		// 		dateCreated: 'asc',
-		// 	}
-		// })
-		// return res.send(messages)
-	return res.send('wow')
+		const messages = await prisma.messages.findMany({
+			take: 30,
+			where: {
+				group: {
+					name: {
+						equals: req.params.room
+					}
+				}
+			},
+			select: {
+				content: true,
+				dateCreated: true,
+				user: {
+					select: {
+						name: true
+					}
+				}
+			},
+			orderBy: {
+				dateCreated: 'asc',
+			}
+		})
+		return res.send(messages)
+	// return res.send('wow')
 	}
 )
 
