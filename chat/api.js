3aa58dcfdd8abbce21e81edd7034b16425941e68
Faisal <adelyarfaisal@gmail.com>
2021-06-03 13:33:17 +0430

    chat route added

--- chat/api.js
+++ chat/api.js
@@ -1,118 +1,118 @@
-const { PrismaClient } = require("@prisma/client");
+const {PrismaClient} = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
 const isGroupExist = async function (groupName) {
-   const group = await prisma.group.findFirst({
-      where: { name: groupName },
-   });
-   return group ? true : false;
+	const group = await prisma.group.findFirst({
+		where: {name: groupName},
+	});
+	return group ? true : false;
 };
 
 const join = async function (roomName, userName) {
-   if (await isGroupExist(roomName)) {
-      await prisma.group.update({
-         where: {
-            name: roomName,
-         },
-         data: {
-            users: {
-               connect: {
-                  name: userName,
-               },
-            },
-         },
-      });
-   } else {
-      await prisma.group.create({
-         data: {
-            name: roomName,
-            users: {
-               connect: {
-                  name: userName,
-               },
-            },
-         },
-      });
-   }
+	if (await isGroupExist(roomName)) {
+		await prisma.group.update({
+			where: {
+				name: roomName,
+			},
+			data: {
+				users: {
+					connect: {
+						name: userName,
+					},
+				},
+			},
+		});
+	} else {
+		await prisma.group.create({
+			data: {
+				name: roomName,
+				users: {
+					connect: {
+						name: userName,
+					},
+				},
+			},
+		});
+	}
 };
 
 const leave = async function (roomName, userName) {
-   await prisma.group.update({
-      where: {
-         name: roomName,
-      },
-      data: {
-         users: {
-            disconnect: {
-               name: userName,
-            },
-         },
-      },
-   });
+	await prisma.group.update({
+		where: {
+			name: roomName,
+		},
+		data: {
+			users: {
+				disconnect: {
+					name: userName,
+				},
+			},
+		},
+	});
 };
 
 const getGroupContent = async function (roomName) {
-   const group = await prisma.group.findMany({
-      include: {
-         users: true,
-      },
-   });
-   return group;
+	const group = await prisma.group.findMany({
+		include: {
+			users: true,
+		},
+	});
+	return group;
 };
 
 const isLastUser = async function (roomName) {
-   const group = await prisma.group.findFirst({
-      where: {
-         name: roomName,
-      },
-      include: {
-         users: true,
-      },
-   });
-   if (group.users.length === 0) {
-      try {
-         await prisma.group.delete({
-            where: {
-               name: roomName,
-            },
-         });
-      } catch (e) {
-         return;
-      }
-   }
+	const group = await prisma.group.findFirst({
+		where: {
+			name: roomName,
+		},
+		include: {
+			users: true,
+		},
+	});
+	if (group.users.length === 0) {
+		try {
+			await prisma.group.delete({
+				where: {
+					name: roomName,
+				},
+			});
+		} catch (e) {
+			return;
+		}
+	}
 };
 
 const createMessage = async function (content, groupName, userName) {
-   const message = await prisma.message.create({
-      data: {
-         content: content,
-         user: {
-            connect: {
-               name: userName,
-            },
-         },
-         group: {
-            connect: {
-               name: groupName,
-            },
-         },
-      },
-      include: {
-         user: {
-            select: {
-               name: true,
-            },
-         },
-      },
-   });
-   return message;
+	const message = await prisma.message.create({
+		data: {
+			content: content,
+			user: {
+				connect: {
+					name: userName,
+				},
+			},
+			group: groupName ? {
+				connect: {
+					name: groupName,
+				},
+			} : null
+		},
+		include: {
+			user: {
+				select: {
+					name: true,
+				},
+			},
+		},
+	});
+	return message;
 };
 
 module.exports = {
-   join,
-   leave,
-   getGroupContent,
-   isLastUser,
-   createMessage,
+	join,
+	leave,
+	getGroupContent,
+	isLastUser,
+	createMessage,
 };
